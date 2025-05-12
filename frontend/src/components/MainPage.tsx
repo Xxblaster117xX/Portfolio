fff6bfbede1dc28a0c4398641ef5e95163d430f5
Xxblaster117xX <gerrero1944@gmail.com>
2025-05-12 17:05:14 +0200

    paquete para transiciones descargado y transición acoplada en página principal

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -1,13 +1,18 @@
-
 import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import '../styles/App.css'; // Asegúrate de que este archivo exista
 
 export default function MainPage() {
   const navigate = useNavigate(); 
 
   return (
-    <div className="app-background">
-      
+    <motion.div
+      className="app-background"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <nav className="navbar">
         <h1>Gestión Almacén</h1>
         <div className="nav-buttons">
@@ -16,10 +21,9 @@ export default function MainPage() {
         </div>
       </nav>
 
-      
       <div className="main-content">
         <h1>Bienvenido al sistema de almacén</h1>
       </div>
-    </div>
+    </motion.div>
   );
 }
